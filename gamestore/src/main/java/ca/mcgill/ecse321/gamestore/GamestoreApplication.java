81728cdd17615143cf323cca32f2ade8b321776b
Laurier Gauvin <57292018+Dummdood@users.noreply.github.com>
2024-09-29 16:47:41 -0400

    Added Spring Boot Project Autogenerated Code

---
+++ gamestore/src/main/java/ca/mcgill/ecse321/gamestore/GamestoreApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.gamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamestoreApplication.class, args);
+	}
+
+}
---
+++ gamestore/src/test/java/ca/mcgill/ecse321/gamestore/GamestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.gamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
