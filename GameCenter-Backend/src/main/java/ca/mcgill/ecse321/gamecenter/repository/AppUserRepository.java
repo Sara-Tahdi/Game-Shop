bade63b42a147d2bf564db09b55ea4cb92fbffc4
tremj <tremerjustin@gmail.com>
2024-10-06 19:50:26 -0400

    more exhaustive tests + use Optional

--- GameCenter-Backend/src/main/java/ca/mcgill/ecse321/gamecenter/repository/AppUserRepository.java
+++ GameCenter-Backend/src/main/java/ca/mcgill/ecse321/gamecenter/repository/AppUserRepository.java
@@ -3,8 +3,12 @@ package ca.mcgill.ecse321.gamecenter.repository;
 import ca.mcgill.ecse321.gamecenter.model.AppUser;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
-    AppUser findAppUserById(int id);
+import java.util.Optional;
 
-    AppUser findAppUserByUsername(String username);
+public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
+    Optional<AppUser> findAppUserById(int id);
+
+    Optional<AppUser> findAppUserByUsername(String username);
+
+    Optional<AppUser> findAppUserByEmail(String email);
 }
--- GameCenter-Backend/src/test/java/ca/mcgill/ecse321/gamecenter/AppUserTests.java
+++ GameCenter-Backend/src/test/java/ca/mcgill/ecse321/gamecenter/AppUserTests.java
@@ -24,64 +24,141 @@ public class AppUserTests {
     }
 
     @Test
-    public void testPersistAndLoadOwnerById() {
+    public void testPersistAndLoadOwner() {
         Owner owner = new Owner();
         owner.setEmail("fakeemail@jmail.ca");
         owner.setPassword("strongpassword123");
         owner.setUsername("TheLegend27");
 
         owner = appUserRepository.save(owner);
+        assertNotNull(owner);
+
         int id = owner.getId();
+        String username = owner.getUsername();
         String email = owner.getEmail();
         String password = owner.getPassword();
 
-        AppUser ownerFromDb = appUserRepository.findAppUserById(id);
+        // Get by ID tests
+        AppUser ownerFromDb = appUserRepository.findAppUserById(id).orElse(null);
+        assertNotNull(ownerFromDb);
 
-        assertNotNull(owner);
         assert((owner.getIsActive() == ownerFromDb.getIsActive()) == true);
         assertEquals(id, ownerFromDb.getId());
+        assertEquals(username, ownerFromDb.getUsername());
+        assertEquals(email, ownerFromDb.getEmail());
+        assertEquals(password, ownerFromDb.getPassword());
+
+        // Get by username tests
+        ownerFromDb = appUserRepository.findAppUserByUsername(username).orElse(null);
+        assertNotNull(ownerFromDb);
+
+        assert((owner.getIsActive() == ownerFromDb.getIsActive()) == true);
+        assertEquals(id, ownerFromDb.getId());
+        assertEquals(username, ownerFromDb.getUsername());
+        assertEquals(email, ownerFromDb.getEmail());
+        assertEquals(password, ownerFromDb.getPassword());
+
+        // Get by email tests
+        ownerFromDb = appUserRepository.findAppUserByEmail(email).orElse(null);
+        assertNotNull(ownerFromDb);
+
+        assert((owner.getIsActive() == ownerFromDb.getIsActive()) == true);
+        assertEquals(id, ownerFromDb.getId());
+        assertEquals(username, ownerFromDb.getUsername());
         assertEquals(email, ownerFromDb.getEmail());
         assertEquals(password, ownerFromDb.getPassword());
     }
 
-    public void testPersistAndLoadEmployeeById() {
+    public void testPersistAndLoadEmployee() {
         Employee employee = new Employee();
         employee.setEmail("bestintern@google.ca");
         employee.setPassword("pushToProd");
         employee.setUsername("Rotmaxx");
 
         employee = appUserRepository.save(employee);
+        assertNotNull(employee);
+
         int id = employee.getId();
+        String username = employee.getUsername();
         String email = employee.getEmail();
         String password = employee.getPassword();
 
-        AppUser employeeFromDb = appUserRepository.findAppUserById(id);
+        // Get by ID tests
+        AppUser employeeFromDb = appUserRepository.findAppUserById(id).orElse(null);
+        assertNotNull(employeeFromDb);
 
-        assertNotNull(employee);
         assert((employee.getIsActive() == employeeFromDb.getIsActive()) == true);
         assertEquals(id, employeeFromDb.getId());
+        assertEquals(username, employeeFromDb.getUsername());
+        assertEquals(email, employeeFromDb.getEmail());
+        assertEquals(password, employeeFromDb.getPassword());
+
+        // Get by username tests
+        employeeFromDb = appUserRepository.findAppUserByUsername(username).orElse(null);
+        assertNotNull(employeeFromDb);
+
+        assert((employee.getIsActive() == employeeFromDb.getIsActive()) == true);
+        assertEquals(id, employeeFromDb.getId());
+        assertEquals(username, employeeFromDb.getUsername());
+        assertEquals(email, employeeFromDb.getEmail());
+        assertEquals(password, employeeFromDb.getPassword());
+
+        // Get by email tests
+        employeeFromDb = appUserRepository.findAppUserByEmail(email).orElse(null);
+        assertNotNull(employeeFromDb);
+
+        assert((employee.getIsActive() == employeeFromDb.getIsActive()) == true);
+        assertEquals(id, employeeFromDb.getId());
+        assertEquals(username, employeeFromDb.getUsername());
         assertEquals(email, employeeFromDb.getEmail());
         assertEquals(password, employeeFromDb.getPassword());
     }
 
     @Test
-    public void testPersistAndLoadClientById() {
+    public void testPersistAndLoadClient() {
         Client client = new Client();
         client.setEmail("bestintern@google.ca");
         client.setPassword("pushToProd");
         client.setUsername("Rotmaxx");
 
         client = appUserRepository.save(client);
+        assertNotNull(client);
+
         int id = client.getId();
+        String username = client.getUsername();
         String email = client.getEmail();
         String password = client.getPassword();
 
-        AppUser clientFromDb = appUserRepository.findAppUserById(id);
+        // Get by ID tests
+        AppUser clientFromDb = appUserRepository.findAppUserById(id).orElse(null);
+        assertNotNull(clientFromDb);
 
-        assertNotNull(client);
         assert((client.getIsActive() == clientFromDb.getIsActive()) == true);
         assertEquals(id, clientFromDb.getId());
+        assertEquals(username, clientFromDb.getUsername());
+        assertEquals(email, clientFromDb.getEmail());
+        assertEquals(password, clientFromDb.getPassword());
+
+        // Get by username tests
+        clientFromDb = appUserRepository.findAppUserByUsername(username).orElse(null);
+        assertNotNull(clientFromDb);
+
+        assert((client.getIsActive() == clientFromDb.getIsActive()) == true);
+        assertEquals(id, clientFromDb.getId());
+        assertEquals(username, clientFromDb.getUsername());
+        assertEquals(email, clientFromDb.getEmail());
+        assertEquals(password, clientFromDb.getPassword());
+
+        // Get by username tests
+        clientFromDb = appUserRepository.findAppUserByEmail(email).orElse(null);
+        assertNotNull(clientFromDb);
+
+        assert((client.getIsActive() == clientFromDb.getIsActive()) == true);
+        assertEquals(id, clientFromDb.getId());
+        assertEquals(username, clientFromDb.getUsername());
         assertEquals(email, clientFromDb.getEmail());
         assertEquals(password, clientFromDb.getPassword());
     }
+
+
 }
