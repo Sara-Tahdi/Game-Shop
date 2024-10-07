6c9c78400b06af19cf07edbd389114db277497d8
tremj <tremerjustin@gmail.com>
2024-10-06 23:19:03 -0400

    passing all staff tests

---
+++ GameCenter-Backend/src/main/java/ca/mcgill/ecse321/gamecenter/repository/StaffRepository.java
@@ -0,0 +1,28 @@
+package ca.mcgill.ecse321.gamecenter.repository;
+
+import ca.mcgill.ecse321.gamecenter.model.AppUser;
+import ca.mcgill.ecse321.gamecenter.model.Staff;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StaffRepository extends CrudRepository<Staff, Integer> {
+    Optional<Staff> findStaffById(int id);
+
+    Optional<Staff> findStaffByUsername(String username);
+
+    Optional<Staff> findStaffByEmail(String email);
+
+    @Query("SELECT a FROM AppUser a WHERE TYPE(a) = :type")
+    Optional<List<AppUser>> findAppUserByUserType(@Param("type") Class<?> type);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE AppUser a SET a.isActive = false WHERE a.username = :username")
+    void updateByUsername(@Param("username") String username);
+}
