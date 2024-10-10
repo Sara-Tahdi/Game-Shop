0fd2f08c6a8c3419b8640d82780bf75a6c5b9e0b
Lenovo-PC-2 <43526682+ltJustWorks@users.noreply.github.com>
2024-10-10 17:36:03 -0400

    add Owner crud repository

---
+++ GameCenter-Backend/src/main/java/ca/mcgill/ecse321/gamecenter/repository/OwnerRepository.java
@@ -0,0 +1,28 @@
+package ca.mcgill.ecse321.gamecenter.repository;
+
+import ca.mcgill.ecse321.gamecenter.model.Owner;
+import ca.mcgill.ecse321.gamecenter.model.Owner;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OwnerRepository extends CrudRepository<Owner, Integer> {
+    Optional<Owner> findOwnerById(int id);
+
+    Optional<Owner> findOwnerByUsername(String username);
+
+    Optional<Owner> findOwnerByEmail(String email);
+
+    @Query("SELECT a FROM AppUser a WHERE TYPE(a) = :type")
+    Optional<List<Owner>> findOwnerByUserType(@Param("type") Class<?> type);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE AppUser a SET a.isActive = false WHERE a.username = :username")
+    void updateByUsername(@Param("username") String username);
+}
