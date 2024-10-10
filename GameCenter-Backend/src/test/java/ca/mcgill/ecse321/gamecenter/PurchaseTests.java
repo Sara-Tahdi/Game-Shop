006b2983d203b4ec4202a285ccb094832022182d
Lenovo-PC-2 <43526682+ltJustWorks@users.noreply.github.com>
2024-10-10 19:37:28 -0400

    add UserPurchasesGame test to Purchase tests

--- GameCenter-Backend/src/test/java/ca/mcgill/ecse321/gamecenter/PurchaseTests.java
+++ GameCenter-Backend/src/test/java/ca/mcgill/ecse321/gamecenter/PurchaseTests.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -22,6 +23,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class PurchaseTests {
     @Autowired
     PurchaseRepository purchaseRepository;
+    @Autowired
+    private ClientRepository clientRepository;
+    @Autowired
+    private GameRepository gameRepository;
 
     @BeforeEach
     @AfterEach
@@ -39,5 +44,46 @@ public class PurchaseTests {
         assertNotNull(purchaseFromDb);
 
         assertEquals(purchase.getId(), purchaseFromDb.getId());
+    }
+
+    void testUserPurchasesGame() {
+        Client client = new Client();
+        client.setEmail("progamer@hai.ca");
+        client.setPassword("mariobros");
+        client.setUsername("Bowser");
+        client = clientRepository.save(client);
+        assertNotNull(client);
+
+        Game game = new Game();
+        game.setPrice(Float.parseFloat("13.99"));
+        game.setRemainingQuantity(10);
+        game = gameRepository.save(game);
+        assertNotNull(game);
+
+        Purchase purchase = new Purchase();
+        purchase.setTotalPrice(game.getPrice());
+        purchase.setCopies(1);
+        purchase.setPurchaseDate(Date.valueOf(LocalDate.now()));
+        purchase.setTrackingCode(3513531);
+        purchase = purchaseRepository.save(purchase);
+        assertNotNull(purchase);
+
+        purchase.setGame(game);
+
+        purchase = purchaseRepository.save(purchase);
+        assertNotNull(purchase);
+
+        client.addPurchaseHistory(purchase);
+
+        client = clientRepository.save(client);
+        assertNotNull(client);
+
+        Purchase purchaseFromDb = purchaseRepository.findPurchaseById(purchase.getId()).orElse(null);
+        assertNotNull(purchaseFromDb);
+
+        assertEquals(purchase.getId(), purchaseFromDb.getId());
+
+        Game gameFromPurchaseFromDb = purchaseFromDb.getGame();
+        assertEquals(game.getId(), gameFromPurchaseFromDb .getId());
     }
 }
