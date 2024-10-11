aad2b79e4cf8e2f4107cc40fa2838d2c1ec635b4
Lenovo-PC-2 <43526682+ltJustWorks@users.noreply.github.com>
2024-10-11 11:13:35 -0400

    add more info to Promotion test

--- GameCenter-Backend/src/test/java/ca/mcgill/ecse321/gamecenter/PromotionTests.java
+++ GameCenter-Backend/src/test/java/ca/mcgill/ecse321/gamecenter/PromotionTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.sql.Date;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -17,6 +19,11 @@ public class PromotionTests {
     @Test
     void testLoadAndPersistPromotion() {
         Promotion promotion = new Promotion();
+        promotion.setNewPrice(23.99F);
+        Date startDate = new Date(1728659221);
+        Date endDate = new Date(1728659221 + 1000 * 60 * 60 * 24);
+        promotion.setStartDate(startDate);
+        promotion.setEndDate(endDate);
         promotion = promotionRepository.save(promotion);
         assertNotNull(promotion);
 
@@ -24,5 +31,8 @@ public class PromotionTests {
         assertNotNull(promotionFromDb);
 
         assertEquals(promotion.getId(), promotionFromDb.getId());
+        assertEquals(promotion.getNewPrice(), promotionFromDb.getNewPrice());
+        assertEquals(promotion.getStartDate().toLocalDate(), promotionFromDb.getStartDate().toLocalDate());
+        assertEquals(promotion.getEndDate().toLocalDate(), promotionFromDb.getEndDate().toLocalDate());
     }
 }
