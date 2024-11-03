160e6b77d12aedc4b79a1894108aca4fcaee22dd
NEKfromLEB <naji.el-khouri2@mail.mcgill.ca>
2024-11-02 21:05:30 -0400

    Implemented PromotionService and updated PromotionRepository

--- GameCenter-Backend/src/main/java/ca/mcgill/ecse321/gamecenter/repository/PromotionRepository.java
+++ GameCenter-Backend/src/main/java/ca/mcgill/ecse321/gamecenter/repository/PromotionRepository.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.gamecenter.repository;
 
+import ca.mcgill.ecse321.gamecenter.model.Game;
 import ca.mcgill.ecse321.gamecenter.model.Promotion;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -13,4 +14,7 @@ public interface PromotionRepository extends CrudRepository<Promotion, Integer> 
 
     @Query("select p from Promotion p where p.game.id = :gameId")
     Optional<List<Promotion>> findPromotionsByGameId(@Param("gameId") int gameId);
+
+    @Query("SELECT p FROM Promotion p WHERE TYPE(p) = :type")
+    Optional<List<Promotion>> findPromotionByPromotionType(@Param("type") Class<?> type);
 }
