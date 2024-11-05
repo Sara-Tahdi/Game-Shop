29dd8964603698d9b13ff64388069c6db07c66fe
Laurier Gauvin <laurier.gauvin@mail.mcgill.ca>
2024-11-05 11:29:56 -0500

    Finished all request related DTOs

--- GameCenter-Backend/src/main/java/ca/mcgill/ecse321/gamecenter/dto/Requests/GameRequestRequestDTO.java
+++ GameCenter-Backend/src/main/java/ca/mcgill/ecse321/gamecenter/dto/Requests/GameRequestRequestDTO.java
@@ -1,5 +1,23 @@
 package ca.mcgill.ecse321.gamecenter.dto.Requests;
 
+import jakarta.validation.constraints.NotBlank;
+
+
 public class GameRequestRequestDTO {
-    
+    @NotBlank(message = "")
+    private String reason;
+    @NotBlank(message = "Creator's username is required")
+    private String createdRequestUsername;
+    @NotBlank(message = "Game title is required")
+    private String gameTitle;
+
+    public GameRequestRequestDTO(String reason, String createdRequestUsername, String gameTitle) {
+        this.reason = reason;
+        this.createdRequestUsername = createdRequestUsername;
+        this.gameTitle = gameTitle;
+    }
+
+    public String getReason() { return this.reason; }
+    public String getCreatedRequestUsername() { return this.createdRequestUsername; }
+    public String getGameTitle() { return this.gameTitle; }    
 }
