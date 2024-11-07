2aed0d820863f10099cbdd541bece2fdf83481cc
Lenovo-PC-2 <43526682+ltJustWorks@users.noreply.github.com>
2024-11-06 20:37:03 -0500

    finish ReviewService tests

--- GameCenter-Backend/src/test/java/ca/mcgill/ecse321/gamecenter/service/ReviewServiceTests.java
+++ GameCenter-Backend/src/test/java/ca/mcgill/ecse321/gamecenter/service/ReviewServiceTests.java
@@ -11,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -88,6 +89,109 @@ public class ReviewServiceTests {
 
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> reviewService.findReviewById(r.getId()));
         assertEquals(e.getMessage(), "No review with id: " + r.getId());
+    }
 
+    @Test
+    public void testCreateReview() {
+        String category = "Adventure";
+        GameCategory gameCategory = new GameCategory(category);
+        when(gameCategoryRepository.save(any(GameCategory.class))).thenReturn(gameCategory);
+        GameCategory savedCategory = gameCategoryService.createGameCategory(category);
+
+        String title = "Minecraft";
+        float price = 26.99f;
+        String description = "block game";
+        Game.GeneralFeeling generalFeeling = Game.GeneralFeeling.VERYPOSITIVE;
+        Game g = new Game(title, price, description, 0, 0, false, generalFeeling, savedCategory);
+        when(gameRepository.save(any(Game.class))).thenReturn(g);
+        when(gameRepository.findGameById(g.getId())).thenReturn(Optional.of(g));
+
+        String author = "awesomesauce3247";
+        String reviewContent = "yes yes this good game, i like";
+        String managerReply = "";
+        Review.Rating stars = Review.Rating.TWO;
+        Review r = new Review(author, reviewContent, managerReply, stars, 0, 0, g);
+
+        when(reviewRepository.save(any(Review.class))).thenReturn(r);
+        Review createdReview = reviewService.createReview(author, reviewContent, managerReply, stars, g);
+
+        assertNotNull(createdReview);
+        assertEquals(createdReview.getId(), r.getId());
+    }
+
+    @Test
+    public void testFindReviewsByGame() {
+        String category = "Adventure";
+        GameCategory gameCategory = new GameCategory(category);
+        when(gameCategoryRepository.save(any(GameCategory.class))).thenReturn(gameCategory);
+        GameCategory savedCategory = gameCategoryService.createGameCategory(category);
+
+        String title = "Minecraft";
+        float price = 26.99f;
+        String description = "block game";
+        Game.GeneralFeeling generalFeeling = Game.GeneralFeeling.VERYPOSITIVE;
+        Game g = new Game(title, price, description, 0, 0, false, generalFeeling, savedCategory);
+        when(gameRepository.save(any(Game.class))).thenReturn(g);
+        when(gameRepository.findGameById(g.getId())).thenReturn(Optional.of(g));
+        Game createdGame = gameService.createGame(title, price, description, generalFeeling, gameCategory);
+
+        String author = "awesomesauce3247";
+        String reviewContent = "yes yes this good game, i like";
+        String managerReply = "";
+        Review.Rating stars = Review.Rating.TWO;
+        Review r = new Review(author, reviewContent, managerReply, stars, 0, 0, g);
+
+        when(reviewRepository.save(any(Review.class))).thenReturn(r);
+        when(reviewRepository.findReviewById(r.getId())).thenReturn(Optional.of(r));
+        Review createdReview = reviewService.createReview(author, reviewContent, managerReply, stars, g);
+        assertNotNull(createdReview);
+
+        when(reviewRepository.findReviewsByGameId(createdGame.getId())).thenReturn(Optional.of(List.of(createdReview)));
+
+        List<Review> foundReviews = reviewService.findReviewsByGame(createdGame.getId());
+        assertNotNull(foundReviews);
+        assertEquals(foundReviews.size(), 1);
+        assertEquals(foundReviews.getFirst().getId(), createdReview.getId());
+    }
+
+    @Test
+    public void testFindReviewsForGameWithNoReviews() {
+        String category = "Adventure";
+        GameCategory gameCategory = new GameCategory(category);
+        when(gameCategoryRepository.save(any(GameCategory.class))).thenReturn(gameCategory);
+        GameCategory savedCategory = gameCategoryService.createGameCategory(category);
+
+        String title = "Minecraft";
+        float price = 26.99f;
+        String description = "block game";
+        Game.GeneralFeeling generalFeeling = Game.GeneralFeeling.VERYPOSITIVE;
+        Game g = new Game(title, price, description, 0, 0, false, generalFeeling, savedCategory);
+        when(gameRepository.save(any(Game.class))).thenReturn(g);
+        when(gameRepository.findGameById(g.getId())).thenReturn(Optional.of(g));
+        Game createdGame = gameService.createGame(title, price, description, generalFeeling, gameCategory);
+
+        when(reviewRepository.findReviewsByGameId(createdGame.getId())).thenReturn(Optional.empty());
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> reviewService.findReviewsByGame(createdGame.getId()));
+        assertEquals(e.getMessage(), "Game with id: " + g.getId() + " has no reviews");
+    }
+
+    @Test
+    public void testFindReviewsForNonexistentGame() {
+        String category = "Adventure";
+        GameCategory gameCategory = new GameCategory(category);
+        when(gameCategoryRepository.save(any(GameCategory.class))).thenReturn(gameCategory);
+        GameCategory savedCategory = gameCategoryService.createGameCategory(category);
+
+        String title = "Minecraft";
+        float price = 26.99f;
+        String description = "block game";
+        Game.GeneralFeeling generalFeeling = Game.GeneralFeeling.VERYPOSITIVE;
+        Game g = new Game(title, price, description, 0, 0, false, generalFeeling, savedCategory);
+        when(gameRepository.save(any(Game.class))).thenReturn(g);
+        when(gameRepository.findGameById(g.getId())).thenReturn(Optional.empty());
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> reviewService.findReviewsByGame(g.getId()));
+        assertEquals(e.getMessage(), "No game with id: " + g.getId());
     }
 }
