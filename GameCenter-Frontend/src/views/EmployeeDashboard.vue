77a5e4c4eb142bfdf228fb660c692944e3aa0c96
Laurier Gauvin <laurier.gauvin@mail.mcgill.ca>
2024-11-29 17:13:33 -0500

    Added client management component so that staff can flag users

--- GameCenter-Frontend/src/views/EmployeeDashboard.vue
+++ GameCenter-Frontend/src/views/EmployeeDashboard.vue
@@ -1,15 +1,18 @@
 <template>
   <GameCategoryManagement />
   <GameManagement />
+  <ClientManagement />
 </template>
 
 <script>
 import GameCategoryManagement from "../components/GameCategoryManagement.vue";
 import GameManagement from "../components/GameManagement.vue";
+import ClientManagement from "../components/ClientManagement.vue";
 export default {
   components: {
     GameCategoryManagement,
     GameManagement,
+    ClientManagement,
   },
 };
 </script>
